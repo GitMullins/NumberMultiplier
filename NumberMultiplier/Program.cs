c0dc60a47cf23bb3dbae0960d2b8d01ac00cdbf9
David Mullins <Mullins6323@gmail.com>
2019-09-01 18:46:25 -0500

    complete

--- NumberMultiplier/Program.cs
+++ NumberMultiplier/Program.cs
@@ -8,7 +8,12 @@ namespace NumberMultiplier
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Would you like to multiply or square your numbers?");
+            var decision = Console.ReadLine();
+
+            Console.WriteLine("Input numbers; seperate each number using a comma");
             var input = Console.ReadLine();
+
             string[] stringNumbers = input.Split(",");
             List<int> numbersList = new List<int>();
 
@@ -18,9 +23,27 @@ namespace NumberMultiplier
                 numbersList.Add(numero);
             }
 
-            int r = numbersList.Aggregate((a, x) => a * x);
+            if (decision == "multiply")
+            {
+                int multipliedTotal = numbersList.Aggregate((a, x) => a * x);
+                Console.WriteLine(multipliedTotal);
+            }
 
-            Console.WriteLine(r);
+            if (decision == "square")
+            {
+                int[] numeros = numbersList.ToArray();
+                var squaredArray = numeros.Select(x => x * x).ToArray();
+
+                string squaredNum = "";
+
+                foreach (int numero in squaredArray)
+                {
+                    squaredNum += $"{ numero},";
+                    //Console.WriteLine(numero);
+                }
+                Console.WriteLine(squaredNum);
+            }
+
         }
     }
 }
