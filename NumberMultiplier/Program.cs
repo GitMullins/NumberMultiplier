79690e67a085d94d3dfa4b66edab60630cb1ebb4
David Mullins <Mullins6323@gmail.com>
2019-09-01 18:02:35 -0500

    multiplication working

---
+++ NumberMultiplier/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            string[] stringNumbers = input.Split(",");
+            List<int> numbersList = new List<int>();
+
+            foreach (var stringNumber in stringNumbers)
+            {
+                int numero = int.Parse(stringNumber);
+                numbersList.Add(numero);
+            }
+
+            int r = numbersList.Aggregate((a, x) => a * x);
+
+            Console.WriteLine(r);
+        }
+    }
+}
